4e8602d32255e3109cf2658f19a38d646d416b5c
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-08-02 13:56:57 +0200

    made ping command slower

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -3,8 +3,13 @@ import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Replies with the bot\'s ping!'),
     async execute(interaction: CommandInteraction) {
-        await interaction.reply('Pong!');
+        await interaction.deferReply();
+        const reply = await interaction.fetchReply();
+        const totalLatency = reply.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(
+            `🏓 Pong!\`\`\`Total latency: ${totalLatency}ms\nAPI latency: ${interaction.client.ws.ping}ms\`\`\``
+        );
     },
 };
