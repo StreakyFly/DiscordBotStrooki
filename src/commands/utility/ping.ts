35418a80c5bcc86ea902c2a1ff0545fbf55bd024
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-08-02 14:27:58 +0200

    made ping command even slower

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
+import db from '../../database';
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,8 +9,15 @@ module.exports = {
         await interaction.deferReply();
         const reply = await interaction.fetchReply();
         const totalLatency = reply.createdTimestamp - interaction.createdTimestamp;
+
+        // Measure database latency
+        const dbStart = Date.now();
+        await db.sequelize.authenticate();
+        const dbEnd = Date.now();
+        const dbLatency = dbEnd - dbStart;
+
         await interaction.editReply(
-            `🏓 Pong!\`\`\`Total latency: ${totalLatency}ms\nAPI latency: ${interaction.client.ws.ping}ms\`\`\``
+            `🏓 Pong!\`\`\`Total latency: ${totalLatency}ms\nAPI latency: ${interaction.client.ws.ping}ms\nDatabase latency: ${dbLatency}ms\`\`\``
         );
     },
 };
